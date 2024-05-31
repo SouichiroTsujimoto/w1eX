a9528d7d4f7c0c37ca165200114eee671b30c0d7
圏内 <souichiroup@protonmail.com>
2024-05-31 22:11:19 +0900

    indent & anchor link

--- src/index.ts
+++ src/index.ts
@@ -23,17 +23,31 @@ async function saveFile(data: string): Promise<void> {
 }
 // パーサーの定義
 const parser = P.createLanguage({
-    Expression: (r) => P.alt(r.Text, r.SharpExpression).many().tieWith(""),
-    Text: (r) => P.alt(P.regex(/[^\$\#\n\r]+/), r.DollarExpression, r.NewLine), 
-    NewLine: () => P.regexp(/[\n\r]/).map((nr) => '<br>'),
+    Note: (r) => P.alt(r.SharpExpression, r.Text).many().tieWith(""),
+    Text: (r) => P.alt(P.regex(/[^\$\#\n\r\!@\}\]]+/), r.AtSignExpression, r.DollarExpression, r.ExclamationExpression, r.NewLine), 
+    NewLine: () => P.regexp(/[\n\r]/).map((nr) => '<br>\n'),
+    AtSignExpression: () => P.seq(
+        P.string('@'),
+        P.regexp(/[^\s]+/),
+    ).map(([at, content]) => `<a href=#${content}>${content}</a>`),
+    ExclamationExpression: (r) => P.alt(r.DefBox), 
+    DefBox: (r) => P.seq(
+        P.regexp(/\!def\s*\(/),
+        P.regexp(/[^\)]*/),
+        P.regexp(/\)(\r*\n*)*/),
+        P.regexp(/\{(\r*\n*)?/),
+        r.Text.many().tieWith(""),
+        P.string('}'),
+    ).map(([def, arg, rp, lc, content, rc]) => `<b id="${arg}">定義(${arg})</b>\n<div class="box">${content}</div>`),
     SharpExpression: (r) => P.seq(
-        P.regexp(/\#+[\s]*/),
+        P.regexp(/\#\s*\{\s*/),
         P.regexp(/[^\n\r]+/),
         P.newline,
-        r.Text.many().tieWith(""),
-    ).map(([start, title, nl, content]) => `<h3>${title}</h3>\n<p>${content}</p>\n`),
+        r.Note,
+        P.string('}'),
+    ).map(([start, title, nl, content, rb]) => `<h3>${title}</h3><div class=box>\n${content}\n</div>`),
     DollarExpression: () => P.seq(
-        P.string('$['),
+        P.regexp(/\$\s*\[/),
         P.regexp(/[^\]]*/), // `]` 以外の任意の文字列
         P.string(']'),
     ).map(([start, content, end]) => `\\(${content}\\)`),
@@ -41,7 +55,7 @@ const parser = P.createLanguage({
 
 // 文字列中の全ての`$[...]`を`\(...\)`に変換する関数
 function transform(input: string): string {
-    const result = parser.Expression.parse(input);
+    const result = parser.Note.parse(input);
     if (result.status) {
         console.log(result);
         return result.value;
@@ -51,19 +65,14 @@ function transform(input: string): string {
 }
 
 loadFile().then((source) => { 
-
-    // 例
-    const inputString = 'This is a $[test] string with $[multiple] $[dollar expressions].';
     const transformedString = transform(source);
     console.log(transformedString);
-
-
 
     let data = `
 <head>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 <style>
-p {
+.box {
 margin-left: 2em;
 }
 </style>
