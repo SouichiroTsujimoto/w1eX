b28c842dd692256abf55eec59395dee08a577c9d
SouichirouTsujimoto <souichiroug@gmail.com>
2024-06-04 13:43:54 +0900

    add exp & change font

--- src/index.ts
+++ src/index.ts
@@ -33,25 +33,38 @@ const parser = P.createLanguage({
         P.string('@'),
         P.regexp(/[^\s]+/),
     ).map(([at, content]) => `<a href=#${content}>${content}</a>`),
-    ExclamationExpression: (r) => P.alt(r.DefBox), 
+    ExclamationExpression: (r) => P.alt(r.DefBox, r.ExpBox), 
     DefBox: (r) => P.seq(
         P.regexp(/\!def\s*\:?/),
         P.regexp(/[^\{]*/),
-        P.regexp(/\s*\{(\r*\n*)?/),
-        r.Text.many().tieWith(""),
-        P.string('}'),
-    ).map(([def, id, lc, content, rc]) =>
-        `<div class="box">\n<b${id.trim()!="" ? " id=\"" + id.trim() + "\"" : ""}><span class="marker">定義${id!="" ? " " + id.trim() : ""}</span></b><br><br>\n${content}</div>`),
+        r.CurlyBracesTexts,
+    ).map(([def, id, content]) =>
+        `<div class="DefBox">\n<b${id.trim()!="" ? " id=\"" + id.trim() + "\"" : ""}><span class="DefMarker">定義${id!="" ? " " + id.trim() : ""}</span></b><br><br>${content}</div>`),
+    ExpBox: (r) => P.seq(
+        P.regexp(/\!exp\s*\:?/),
+        P.regexp(/[^\{]*/),
+        r.CurlyBracesTexts,
+        r.CurlyBracesTexts, // できれば任意にしたい
+    ).map(([exp, id, content1, content2]) =>
+        `<div class="ExpBox">\n<b${id.trim()!="" ? " id=\"" + id.trim() + "\"" : ""}><span class="ExpMarker">例題${id!="" ? " " + id.trim() : ""}</span></b><br><br>${content1}<br><details><summary>解答</summary>${content2}</details></div>`),
     SharpExpression: (r) => P.seq(
         P.regexp(/\#\s*/),
         P.regexp(/[^\{\:]*/),
         P.regexp(/\:?/),
         P.regexp(/[^\{]*/),
+        r.CurlyBracesNote
+    ).map(([start, title, colon, id, cbnote]) =>
+        `<b${id.trim()!="" ? " id=\"" + id.trim() + "\"" : ""}>${id.trim()!="" ? id.trim() + " " : ""} ${title}</b>${cbnote}`),
+    CurlyBracesTexts: (r) => P.seq(
+        P.regexp(/\s*\{\s*/),
+        r.Text.many().tieWith(""),
+        P.string('}'),
+    ).map(([lc, content, rc]) => `\n${content}\n`),
+    CurlyBracesNote: (r) => P.seq(
         P.regexp(/\s*\{\s*/),
         r.Note,
         P.string('}'),
-    ).map(([start, title, colon, id, lb, content, rb]) =>
-        `<b${id.trim()!="" ? " id=\"" + id.trim() + "\"" : ""}>${id.trim()!="" ? id.trim() + " " : ""} ${title}</b><div class="section">\n${content}\n</div>`),
+    ).map(([lc, content, rc]) => `<div class="section">\n${content}\n</div>`),
     DollarExpression: () => P.seq(
         P.regexp(/\$\s*\[\s*/),
         P.regexp(/[^\]]*/), // `]` 以外の任意の文字列
@@ -67,7 +80,8 @@ function transform(input: string): string {
         console.log(result);
         return result.value;
     } else {
-        throw new Error(`Parsing failed: ${result.expected} | ${result.index.offset}`);
+        console.log(`Parsing failed: ${result.expected} | ${result.index.offset}`);
+        return `<div>Parsing failed: ${result.expected} | ${result.index.offset}</div>`;
     }
 }
 
@@ -80,7 +94,9 @@ loadFile().then((source) => {
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 <style>
 body{
-font-family: "Hiragino Sans";
+line-break: anywhere;
+background-color: white;
+color: black;
 font-size:16px;
 line-height:100%;
 box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
@@ -96,7 +112,7 @@ border-radius: 5px;
 padding-top: 10px;
 padding-left: 14px;
 }
-.box {
+.DefBox {
 font-size:16px;
 line-height:100%;
 box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
@@ -104,8 +120,19 @@ margin-top: 7px;
 border-radius: 5px;
 padding: 14px;
 }
-span.marker {
-    background: linear-gradient(transparent 40%, #a5dee5 40% 80%, transparent 80%);
+.ExpBox {
+font-size:16px;
+line-height:100%;
+box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+margin-top: 7px;
+border-radius: 5px;
+padding: 14px;
+}
+span.DefMarker {
+background: linear-gradient(transparent 50%, #a5dee5 50% 100%);
+}
+span.ExpMarker {
+background: linear-gradient(transparent 50%, #fff57d 50% 100%);
 }
 </style>
 </head>
